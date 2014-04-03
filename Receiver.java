9ccf5a92f2965d4ff2df77fa9ce06ca135672e5b
gatzka <gatzka@63959f38-955e-4af9-9822-c72031be6dd2>
2014-04-03 20:01:50 +0000

    Adapt Receiver class to use the new StringMessageReceiver.
    git-svn-id: http://repository.devel.hbm.com/svn/device_libs/scan/java-scan/trunk@186 63959f38-955e-4af9-9822-c72031be6dd2

--- Receiver.java
+++ Receiver.java
@@ -1,30 +1,26 @@
-import com.hbm.devices.scan.MulticastReceiver;
+//import com.hbm.devices.scan.MulticastReceiver;
+import com.hbm.devices.scan.StringMessageMulticastReceiver;
 import com.hbm.devices.scan.IPv4ScanInterfaces;
 import com.hbm.devices.scan.messages.JsonRpc;
+import com.hbm.devices.scan.ScanConstants;
 
 import java.util.Observable;
 import java.util.Observer;
+import java.io.IOException;
 
 public class Receiver implements Observer {
 	public static void main(String[] args) {
-		Receiver r = new Receiver();
 		try {
-			IPv4ScanInterfaces ifs = new IPv4ScanInterfaces();
-			MulticastReceiver mrs = new MulticastReceiver(ifs.getInterfaces());
-			mrs.addObserver(r);
-			Thread t = new Thread(mrs);
-			t.start();
+			StringMessageMulticastReceiver smr = new StringMessageMulticastReceiver(ScanConstants.SCAN_ADDRESS, ScanConstants.SCAN_PORT);
+			smr.start();
 			synchronized(args) {
 				args.wait();
 			}
-			mrs.stop();
-			t.join();
 		} catch (Exception e) {
-			System.out.println("got Exception");
-			e.printStackTrace();
 			System.out.println(e);
 		}
 	}
+
 
 	public void update(Observable o, Object arg) {
 		JsonRpc packet = (JsonRpc)arg;
