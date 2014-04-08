5c49defb078c2ba9d9b7bfbe0524c089de4b499b
gatzka <gatzka@63959f38-955e-4af9-9822-c72031be6dd2>
2014-04-08 11:40:29 +0000

    Add unit test environment.
    git-svn-id: http://repository.devel.hbm.com/svn/device_libs/scan/java-scan/trunk@220 63959f38-955e-4af9-9822-c72031be6dd2

---
+++ test/JsonFilterTest.java
@@ -0,0 +1,12 @@
+package com.hbm.devices.scan.test;
+
+import org.junit.* ;
+import static org.junit.Assert.* ;
+
+public class JsonFilterTest {
+
+	@Test
+	public void bla() {
+		assertTrue(0 == 1);
+	}
+}
