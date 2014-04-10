71bbef7fd1c1fb98597ee92dd890c90226a3c997
gatzka <gatzka@63959f38-955e-4af9-9822-c72031be6dd2>
2014-04-10 10:03:31 +0000

    Implement unit test to ensure correct JSON message is handled properly.
    git-svn-id: http://repository.devel.hbm.com/svn/device_libs/scan/java-scan/trunk@224 63959f38-955e-4af9-9822-c72031be6dd2

--- test/JsonFilterTest.java
+++ test/JsonFilterTest.java
@@ -1,12 +1,31 @@
-package com.hbm.devices.scan.test;
-
 import org.junit.* ;
 import static org.junit.Assert.* ;
+
+import com.hbm.devices.scan.AnnouncePath;
+import com.hbm.devices.scan.filter.JsonFilter;
+import com.hbm.devices.scan.FakeStringMessageMulticastReceiver;
+import com.hbm.devices.scan.messages.Announce;
+import java.util.Observable;
+import java.util.Observer;
 
 public class JsonFilterTest {
 
 	@Test
-	public void bla() {
-		assertTrue(0 == 1);
+	public void parseCorrectMessage() {
+		final Closure closure = new Closure();
+		FakeStringMessageMulticastReceiver fsmmr = new FakeStringMessageMulticastReceiver();
+		JsonFilter jf = new JsonFilter();
+		fsmmr.addObserver(jf);
+		jf.addObserver(new Observer(){
+			public void update(Observable o, Object arg) {
+				closure.ap = (AnnouncePath)arg;
+			}
+		});
+		fsmmr.emitSingleCorrectMessage();
+		assertTrue(closure.ap != null);
 	}
+}
+
+class Closure {
+	public AnnouncePath ap;
 }
