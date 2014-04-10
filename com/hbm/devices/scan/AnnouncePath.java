049a97685717282edda0cf030af82603081d5816
gatzka <gatzka@63959f38-955e-4af9-9822-c72031be6dd2>
2014-04-10 10:50:40 +0000

    Correct error handling if information is missing in announce packets.
    git-svn-id: http://repository.devel.hbm.com/svn/device_libs/scan/java-scan/trunk@236 63959f38-955e-4af9-9822-c72031be6dd2

--- com/hbm/devices/scan/AnnouncePath.java
+++ com/hbm/devices/scan/AnnouncePath.java
@@ -11,10 +11,24 @@ public class AnnouncePath {
 		this.announce = announce;
 		StringBuilder sb = new StringBuilder(100);
 		try {
+			String uuid = announce.getParams().getDevice().getUuid();
+			if (uuid == null) {
+				throw new MissingDataException();
+			}
 			sb.append(announce.getParams().getDevice().getUuid());
+
 			Router router = announce.getParams().getRouter();
 			if (router != null) {
+				String routerUuid = router.getUuid();
+				if (routerUuid == null) {
+					throw new MissingDataException();
+				}
 				sb.append(router.getUuid());
+			}
+
+			String interfaceName = announce.getParams().getNetSettings().getInterface().getName();
+			if (interfaceName == null) {
+				throw new MissingDataException();
 			}
 			sb.append(announce.getParams().getNetSettings().getInterface().getName());
 			hash = sb.toString().hashCode();
