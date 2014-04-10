c8ddb83deaceabe2b13596a24912c1714ec50110
gatzka <gatzka@63959f38-955e-4af9-9822-c72031be6dd2>
2014-04-10 10:03:59 +0000

    Add some errorneous JSON messages to be emitted later in a unit test.
    git-svn-id: http://repository.devel.hbm.com/svn/device_libs/scan/java-scan/trunk@230 63959f38-955e-4af9-9822-c72031be6dd2

--- com/hbm/devices/scan/FakeStringMessageMulticastReceiver.java
+++ com/hbm/devices/scan/FakeStringMessageMulticastReceiver.java
@@ -56,6 +56,103 @@ public class FakeStringMessageMulticastReceiver extends Observable {
 		"}" +
 	"}";
 
+	private static final String missingDeviceMessage =
+	"{" +
+		"\"jsonrpc\":\"2.0\",\"method\":\"announce\",\"params\":{" +
+			"\"apiVersion\":\"1.0\"," +
+			"\"expiration\":15," + 
+			"\"netSettings\":{" +
+				"\"defaultGateway\":{\"ipv4Address\":\"172.19.169.254\"}," +
+				"\"interface\":{\"description\":\"ethernet backplane side\"," +
+					"\"ipv4\":[{\"address\":\"172.19.192.57\",\"netmask\":\"255.255.0.0\"}]," + 
+					"\"ipv6\":[{\"address\":\"fe80::209:e5ff:fe00:123a\",\"prefix\":64}]," +
+					"\"name\":\"eth0\",\"type\":\"ethernet\"}"+
+			"}" + 
+		"}" +
+	"}";
+
+	private static final String missingDeviceUuidMessage =
+	"{" +
+		"\"jsonrpc\":\"2.0\",\"method\":\"announce\",\"params\":{" +
+			"\"apiVersion\":\"1.0\",\"device\":{\"familyType\":\"QuantumX\"," + 
+			"\"firmwareVersion\":\"4.1.1.18610.1\",\"hardwareId\":\"MX410_R0\"," +
+			"\"name\":\"MX410 Matthias\",\"type\":\"MX410\"}," +
+			"\"expiration\":15," + 
+			"\"netSettings\":{" +
+				"\"defaultGateway\":{\"ipv4Address\":\"172.19.169.254\"}," +
+				"\"interface\":{\"description\":\"ethernet backplane side\"," +
+					"\"ipv4\":[{\"address\":\"172.19.192.57\",\"netmask\":\"255.255.0.0\"}]," + 
+					"\"ipv6\":[{\"address\":\"fe80::209:e5ff:fe00:123a\",\"prefix\":64}]," +
+					"\"name\":\"eth0\",\"type\":\"ethernet\"}"+
+			"}" + 
+		"}" +
+	"}";
+
+	private static final String missingParamsMessage =
+	"{" +
+		"\"jsonrpc\":\"2.0\",\"method\":\"announce\"" +
+	"}";
+
+	private static final String noInterfaceNameMessage =
+	"{" +
+		"\"jsonrpc\":\"2.0\",\"method\":\"announce\",\"params\":{" +
+			"\"apiVersion\":\"1.0\",\"device\":{\"familyType\":\"QuantumX\"," + 
+			"\"firmwareVersion\":\"4.1.1.18610.1\",\"hardwareId\":\"MX410_R0\"," +
+			"\"name\":\"MX410 Matthias\",\"type\":\"MX410\",\"uuid\":\"0009E500123A\"}," +
+			"\"expiration\":15," + 
+			"\"netSettings\":{" +
+				"\"defaultGateway\":{\"ipv4Address\":\"172.19.169.254\"}," +
+				"\"interface\":{\"description\":\"ethernet backplane side\"," +
+					"\"ipv4\":[{\"address\":\"172.19.192.57\",\"netmask\":\"255.255.0.0\"}]," + 
+					"\"ipv6\":[{\"address\":\"fe80::209:e5ff:fe00:123a\",\"prefix\":64}]," +
+					"\"type\":\"ethernet\"}"+
+			"}" + 
+		"}" +
+	"}";
+
+	private static final String noInterfaceMessage =
+	"{" +
+		"\"jsonrpc\":\"2.0\",\"method\":\"announce\",\"params\":{" +
+			"\"apiVersion\":\"1.0\",\"device\":{\"familyType\":\"QuantumX\"," + 
+			"\"firmwareVersion\":\"4.1.1.18610.1\",\"hardwareId\":\"MX410_R0\"," +
+			"\"name\":\"MX410 Matthias\",\"type\":\"MX410\",\"uuid\":\"0009E500123A\"}," +
+			"\"expiration\":15," + 
+			"\"netSettings\":{" +
+				"\"defaultGateway\":{\"ipv4Address\":\"172.19.169.254\"}" +
+			"}" + 
+		"}" +
+	"}";
+
+	private static final String noNetSettingsMessage =
+	"{" +
+		"\"jsonrpc\":\"2.0\",\"method\":\"announce\",\"params\":{" +
+			"\"apiVersion\":\"1.0\",\"device\":{\"familyType\":\"QuantumX\"," + 
+			"\"firmwareVersion\":\"4.1.1.18610.1\",\"hardwareId\":\"MX410_R0\"," +
+			"\"name\":\"MX410 Matthias\",\"type\":\"MX410\",\"uuid\":\"0009E500123A\"}," +
+			"\"expiration\":15" + 
+		"}" +
+	"}";
+
+	private static final String missingRouterUuid =
+	"{" +
+		"\"jsonrpc\":\"2.0\",\"method\":\"announce\",\"params\":{" +
+			"\"apiVersion\":\"1.0\",\"device\":{\"familyType\":\"QuantumX\"," + 
+			"\"firmwareVersion\":\"4.1.1.18610.1\",\"hardwareId\":\"MX410_R0\"," +
+			"\"name\":\"MX410 Matthias\",\"type\":\"MX410\",\"uuid\":\"0009E500123A\"}," +
+			"\"expiration\":15," + 
+			"\"netSettings\":{" +
+				"\"defaultGateway\":{\"ipv4Address\":\"172.19.169.254\"}," +
+				"\"interface\":{\"description\":\"ethernet backplane side\"," +
+					"\"ipv4\":[{\"address\":\"172.19.192.57\",\"netmask\":\"255.255.0.0\"}]," + 
+					"\"ipv6\":[{\"address\":\"fe80::209:e5ff:fe00:123a\",\"prefix\":64}]," +
+					"\"name\":\"eth0\",\"type\":\"ethernet\"}"+
+			"}," +
+			"\"router\": {" +
+				"\"uuid\": \"0x12345\"" +
+			"}" +
+		"}" +
+	"}";
+
 	public void emitSingleCorrectMessage() {
 		setChanged();
 		notifyObservers(correctMessage);
