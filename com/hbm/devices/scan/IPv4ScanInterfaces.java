7ac13e851496dcf0bff2ead6242777088162faff
gatzka <gatzka@63959f38-955e-4af9-9822-c72031be6dd2>
2014-04-19 21:35:08 +0000

    Add method to determine if a device is connectable over IPv4.
    git-svn-id: http://repository.devel.hbm.com/svn/device_libs/scan/java-scan/trunk@252 63959f38-955e-4af9-9822-c72031be6dd2

--- com/hbm/devices/scan/IPv4ScanInterfaces.java
+++ com/hbm/devices/scan/IPv4ScanInterfaces.java
@@ -6,12 +6,17 @@ package com.hbm.devices.scan;
 
 import java.net.Inet4Address;
 import java.net.InetAddress;
-import java.net.InetAddress;
+import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Collection;
 import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
+
+import com.hbm.devices.scan.messages.IPv4Entry;
 
 public class IPv4ScanInterfaces {
 
@@ -30,7 +35,77 @@ public class IPv4ScanInterfaces {
 	public Collection<NetworkInterface> getInterfaces() {
 		return interfaces;
 	}
+
+	public static InetAddress getConnectableIPv4Address(Collection<NetworkInterface> ni, Iterable<IPv4Entry> addresses) {
+		Iterator<NetworkInterface> interfaceIterator = ni.iterator();
+		while (interfaceIterator.hasNext()) {
+			NetworkInterface iface = interfaceIterator.next();
+			Iterator<IPv4Entry> addressIterator = addresses.iterator();
+			while (addressIterator.hasNext()) {
+				IPv4Entry address = addressIterator.next();
+				InetAddress connectAddress = getConnectAddress(iface, address);
+				if (connectAddress != null) {
+					return connectAddress;
+				}
+			}
+		}
+		return null;
+	}
 	
+	private static InetAddress getConnectAddress(NetworkInterface ni, IPv4Entry address) {
+		InetAddress announceAddress;
+		InetAddress announceNetmask;
+		try {
+			announceAddress = InetAddress.getByName(address.getAddress());
+			announceNetmask = InetAddress.getByName(address.getNetmask());
+		} catch (UnknownHostException e) {
+			return null;
+		}
+
+		List<InterfaceAddress> niAddresses = ni.getInterfaceAddresses();
+		Iterator<InterfaceAddress> niIterator = niAddresses.iterator();
+		while (niIterator.hasNext()) {
+			InterfaceAddress niAddress = niIterator.next();
+			InetAddress interfaceAddress = niAddress.getAddress();
+			if (interfaceAddress instanceof Inet4Address) {
+				short interfacePrefix = niAddress.getNetworkPrefixLength();
+				short announcePrefix = getPrefix(announceNetmask);
+				if (addressMatch(announceAddress, announcePrefix, interfaceAddress, interfacePrefix)) {
+					return announceAddress;
+				}
+			}
+		}
+		return null;
+	}
+	
+	private static short getPrefix(InetAddress announceNetmask) {
+		byte[] address = announceNetmask.getAddress();
+		int prefix = 0;
+		for (int i = 0; i < 4; i++) {
+			prefix += Integer.bitCount(address[i] & 0xff);
+		}
+		return (short)prefix;
+	}
+
+	private static boolean addressMatch(InetAddress announceAddress, short announcePrefix,
+	                                    InetAddress interfaceAddress, short interfacePrefix) {
+		byte[] announceBytes = announceAddress.getAddress();
+		byte[] interfaceBytes = interfaceAddress.getAddress();
+		int announceInteger = convertToInteger(announceBytes);
+		int interfaceInteger = convertToInteger(interfaceBytes);
+		announceInteger = announceInteger >>> (32 - announcePrefix);
+		interfaceInteger = interfaceInteger >>> (32 - interfacePrefix);
+		return announceInteger == interfaceInteger;
+	}
+
+	private static int convertToInteger(byte[] address) {
+		int value = ((((int)address[0]) & 0xff) << 24);
+		value |= ((((int)address[1]) & 0xff) << 16);
+		value |= ((((int)address[2]) & 0xff) << 8);
+		value |= ((((int)address[3]) & 0xff) << 0);
+		return value;
+	}
+
 	private static boolean willScan(NetworkInterface iface) throws SocketException {
 		if (iface.isLoopback()) {
 			return false;
