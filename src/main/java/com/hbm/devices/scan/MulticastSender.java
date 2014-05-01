e10d671eb7ae55df142484e13e206270b78ec6e2
gatzka <gatzka@63959f38-955e-4af9-9822-c72031be6dd2>
2014-05-01 13:39:25 +0000

    Minimize creation of objects.
    git-svn-id: http://repository.devel.hbm.com/svn/device_libs/scan/java-scan/trunk@314 63959f38-955e-4af9-9822-c72031be6dd2

--- src/main/java/com/hbm/devices/scan/MulticastSender.java
+++ src/main/java/com/hbm/devices/scan/MulticastSender.java
@@ -6,6 +6,7 @@ import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.nio.charset.Charset;
 import java.util.Collection;
 import java.util.HashSet;
 
@@ -13,18 +14,23 @@ public class MulticastSender {
 	
 	private MulticastSocket socket;
 	private HashSet<NetworkInterface> multicastSender;
+	private Charset charset;
+	private InetAddress configureAddress;
 
 	public MulticastSender(Collection<NetworkInterface> ifs) throws IOException {
+		charset = Charset.forName("UTF-8");
+		configureAddress = InetAddress.getByName(ScanConstants.SCAN_ADDRESS);
 		socket = new MulticastSocket(ScanConstants.SCAN_PORT);
 		multicastSender = new HashSet<NetworkInterface>();
 		multicastSender.addAll(ifs);
+		// join all interfaces
 	}
 
 	public void sendMessage(String msg) throws IOException, SocketException {
+		byte[] bytes = msg.getBytes(charset);
+		DatagramPacket packet = new DatagramPacket(bytes, bytes.length,	configureAddress, ScanConstants.SCAN_PORT);
 		for (NetworkInterface iface : multicastSender) { 
 			socket.setNetworkInterface(iface);
-			DatagramPacket packet = new DatagramPacket(msg.getBytes(), msg.length(),
-				InetAddress.getByName(ScanConstants.SCAN_ADDRESS), ScanConstants.SCAN_PORT);
 			socket.send(packet);
 		}
 	}
