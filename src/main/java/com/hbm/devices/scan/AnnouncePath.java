fb5a6e9d778e3372b3c3cb88b9bbabbffb720a64
gatzka <gatzka@63959f38-955e-4af9-9822-c72031be6dd2>
2014-05-02 08:27:51 +0000

    Describe purpose of this class.
    git-svn-id: http://repository.devel.hbm.com/svn/device_libs/scan/java-scan/trunk@321 63959f38-955e-4af9-9822-c72031be6dd2

--- src/main/java/com/hbm/devices/scan/AnnouncePath.java
+++ src/main/java/com/hbm/devices/scan/AnnouncePath.java
@@ -2,6 +2,20 @@ package com.hbm.devices.scan;
 
 import com.hbm.devices.scan.messages.*;
 
+/**
+ * This class carries parsed announce messages.
+ * <p>
+ * If it would be possible to retrieve the information which {@link
+ * java.net.NetworkInterface} received an announce messages, this will
+ * also handled here to provide all information necessary to decide if
+ * an IP communication is possible to the announced device.
+ * <p>
+ * Please note that the {@link #hashCode()} method is overridden. The
+ * hash code of this announce object is unique for the communication
+ * path the {@link Announce} message traveled.
+ *
+ * @since 1.0
+ */
 public class AnnouncePath {
 
 	private Announce announce;
