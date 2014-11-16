76801c0352d4fd910d3a3bf3dee493219fa38871
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-11-16 19:53:16 +0100

    Load IP ports from property file.

--- src/main/java/com/hbm/devices/scan/ScanConstants.java
+++ src/main/java/com/hbm/devices/scan/ScanConstants.java
@@ -11,11 +11,11 @@ import java.util.Properties;
  */
 public class ScanConstants {
 
-    public static final String RESPONSE_ADDRESS;
-    public static final int RESPONSE_PORT = 31417;
-
     public static final String ANNOUNCE_ADDRESS;
-    public static final int ANNOUNCE_PORT = 31416;
+    public static final int ANNOUNCE_PORT;
+
+    public static final String RESPONSE_ADDRESS;
+    public static final int RESPONSE_PORT;
 
     public static final String LOGGER_NAME = "scan";
 
@@ -26,8 +26,10 @@ public class ScanConstants {
         try {
             Properties props = new Properties();
             props.load(ClassLoader.getSystemResourceAsStream("scan.properties"));
-            RESPONSE_ADDRESS = props.getProperty("scan.announce.address");
-            ANNOUNCE_ADDRESS = props.getProperty("scan.configure.address");
+            ANNOUNCE_ADDRESS = props.getProperty("scan.announce.address");
+            ANNOUNCE_PORT = Integer.parseInt(props.getProperty("scan.announce.port"));
+            RESPONSE_ADDRESS = props.getProperty("scan.configure.address");
+            RESPONSE_PORT = Integer.parseInt(props.getProperty("scan.configure.port"));
         } catch (IOException e) {
             throw new ExceptionInInitializerError(e);
         }
