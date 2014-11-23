179ee5e5fcafb7dcd760c2d80e84b517e0b69691
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-11-23 15:44:35 +0100

    Add license header to every java file.

--- src/main/java/com/hbm/devices/configure/package-info.java
+++ src/main/java/com/hbm/devices/configure/package-info.java
@@ -1,3 +1,15 @@
+/*
+ * Java Scan, a library for scanning and configuring HBM devices.
+ * Copyright (C) 2014 Stephan Gatzka
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ */
+
 /**
  * Provides classes implementing the 
  * <a href="http://www.hbm.com/" target="_blank">HBM (Hottinger Baldwin Messtechnik GmbH)</a>
