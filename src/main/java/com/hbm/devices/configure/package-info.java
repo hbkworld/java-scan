892556fc3ef363792782b04a022bb9261b22d483
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-11-23 18:16:04 +0100

    Make some lines shorter for better readability.

--- src/main/java/com/hbm/devices/configure/package-info.java
+++ src/main/java/com/hbm/devices/configure/package-info.java
@@ -11,9 +11,9 @@
  */
 
 /**
- * Provides classes implementing the 
- * <a href="http://www.hbm.com/" target="_blank">HBM (Hottinger Baldwin Messtechnik GmbH)</a>
- * device configuration.
+ * Provides classes implementing the <a href="http://www.hbm.com/"
+ * target="_blank">HBM (Hottinger Baldwin Messtechnik GmbH)</a> device
+ * configuration.
  *
  */
 package com.hbm.devices.configure;
