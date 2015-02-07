a118568340335fe062d2105e5524dbb6e68731a5
Stephan Gatzka <stephan.gatzka@gmail.com>
2015-02-07 16:52:14 +0100

    Add new matcher to match for Service entries int announce objects.

---
+++ src/main/java/com/hbm/devices/scan/announce/filter/ServiceFilter.java
@@ -0,0 +1,73 @@
+/*
+ * Java Scan, a library for scanning and configuring HBM devices.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (C) Stephan Gatzka
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.hbm.devices.scan.announce.filter;
+
+import com.hbm.devices.scan.messages.Announce;
+import com.hbm.devices.scan.messages.MissingDataException;
+import com.hbm.devices.scan.messages.ServiceEntry;
+
+/**
+ * This class matches family type information in Announce objects.
+ * <p>
+ *
+ * @since 1.0
+ */
+public final class ServiceFilter implements Matcher {
+
+    private final String[] serviceTypes;
+
+    /**
+     * Creates a {@link Matcher} object that matches if the service
+     * section in the anounce contains an http entry.
+     *
+     * @param serviceTypes An array of {@link String}s used to match
+     * against the service entries in {@link Announce} objects.
+     */
+    public ServiceFilter(String... serviceTypes) {
+        this.serviceTypes = serviceTypes;
+    }
+
+    @Override
+    public boolean match(Announce announce) throws MissingDataException {
+        final Iterable<ServiceEntry> services = announce.getParams().getServices();
+        for (final ServiceEntry entry : services) {
+            for (int i = 0; i < serviceTypes.length; i++) {
+                if (serviceTypes[i].equals(entry.getType())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String[] getFilterStrings() {
+        return serviceTypes.clone();
+    }
+}
