049c565f831afecdcd7ebfb82ebd26a754398519
Stephan Gatzka <stephan.gatzka@gmail.com>
2015-02-08 19:54:09 +0100

    Add test stub to parse a constructed announce message.

--- src/test/java/MessageParserTest.java
+++ src/test/java/MessageParserTest.java
@@ -35,6 +35,10 @@ import java.util.Observer;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 import com.hbm.devices.scan.FakeMessageReceiver;
 import com.hbm.devices.scan.messages.CommunicationPath;
 import com.hbm.devices.scan.messages.MessageParser;
@@ -156,5 +160,89 @@ public class MessageParserTest {
     public void parseMissingTypeMessage() {
         fsmmr.emitMissingTypeResponseMessage();
         assertNull("Got result object from response with error and result", res);
+    }
+
+    @Test
+    public void testGetters() {
+        final String apiVersionString = "1.0";
+        final String familyTypeString = "QuantumX";
+        final String fwVersionString = "1.234";
+        final String hwIdString = "MX410_R0";
+        final String nameString = "MX410 Matthias";
+        final String typeString = "MX410";
+        final String uuidString = "0009E500123A";
+        final String ifaceNameString = "eth0";
+        final String ifaceType = "ethernet";
+        final String gwAddressString = "172.19.169.254";
+        final String ifDescriptionString = "ethernet backplane side";
+        final String ipv4Address = "172.19.192.57";
+        final String ipv4Mask = "255.255.0.0";
+        final String ipv6Address = "fe80::209:e5ff:fe00:123a";
+        final int ipv6Prefix = 64;
+        final int expire = 15;
+        final String routerUUID = "0009E50013E9";
+        final String streamType = "daqStream";
+        final int streamPort = 7411;
+        final String httpType = "http";
+        final int httpPort = 80;
+
+        final JsonObject root = new JsonObject();
+        root.addProperty("jsonrpc", "2.0");
+        root.addProperty("method", "announce");
+        final JsonObject params = new JsonObject();
+        root.add("params", params);
+        params.addProperty("expiration", expire);
+        params.addProperty("apiVersion", apiVersionString);
+        
+        final JsonObject device = new JsonObject();
+        params.add("device", device);
+        device.addProperty("familyType", familyTypeString);
+        device.addProperty("firmwareVersion", fwVersionString);
+        device.addProperty("hardwareId", hwIdString);
+        device.addProperty("name", nameString);
+        device.addProperty("type", typeString);
+        device.addProperty("uuid", uuidString);
+
+        final JsonObject netSettings = new JsonObject();
+        params.add("netSettings", netSettings);
+        final JsonObject defaultGW = new JsonObject();
+        netSettings.add("defaultGateway", defaultGW);
+        defaultGW.addProperty("ipv4Address", gwAddressString);
+        final JsonObject iface = new JsonObject();
+        netSettings.add("interface", iface);
+        iface.addProperty("name", ifaceNameString);
+        iface.addProperty("type", ifaceType);
+        iface.addProperty("description", ifDescriptionString);
+        JsonArray ipv4Addresses = new JsonArray();
+        iface.add("ipv4", ipv4Addresses);
+        final JsonObject ipv4Entry = new JsonObject();
+        ipv4Addresses.add(ipv4Entry);
+        ipv4Entry.addProperty("address", ipv4Address);
+        ipv4Entry.addProperty("netmask", ipv4Mask);
+        JsonArray ipv6Addresses = new JsonArray();
+        iface.add("ipv6", ipv6Addresses);
+        final JsonObject ipv6Entry = new JsonObject();
+        ipv6Addresses.add(ipv6Entry);
+        ipv6Entry.addProperty("address", ipv6Address);
+        ipv6Entry.addProperty("prefix", ipv6Prefix);
+
+        final JsonObject router = new JsonObject();
+        params.add("router", router);
+        router.addProperty("uuid", routerUUID);
+
+        final JsonArray services = new JsonArray();
+        params.add("services", services);
+        JsonObject streamService = new JsonObject();
+        services.add(streamService);
+        streamService.addProperty("type", streamType);
+        streamService.addProperty("port", streamPort);
+        JsonObject httpService = new JsonObject();
+        services.add(httpService);
+        httpService.addProperty("type", httpType);
+        httpService.addProperty("port", httpPort);
+
+        final Gson gson = new Gson();
+        fsmmr.emitString(gson.toJson(root));
+        assertNotNull("No CommunictionPath object after correct message", cp);
     }
 }
