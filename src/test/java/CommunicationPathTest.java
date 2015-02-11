9540819d16b7ef5d216dd7e06d589a11bb30b561
Stephan Gatzka <stephan.gatzka@gmail.com>
2015-02-11 21:19:49 +0100

    Ensure that different CommunicationPath object representing the same communication path are equal.

--- src/test/java/CommunicationPathTest.java
+++ src/test/java/CommunicationPathTest.java
@@ -29,7 +29,9 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -80,9 +82,7 @@ public class CommunicationPathTest {
 
         CommunicationPath oldCp = cp;
         fsmmr.emitCorrectMessageManual();
+        assertNotSame("New object is same as old", oldCp, cp);
         assertEquals("Both CommunicationPath objects represent the same communication path, but return not equal", oldCp, cp);
-
     }
-
-
 }
