9c055032a4e29bb416d03845a3f4120d6183b9a4
Stephan Gatzka <stephan.gatzka@gmail.com>
2015-02-22 15:45:11 +0100

    Add test to intantiate an AnnounceReceiver.

---
+++ src/test/java/AnnounceReceiverTest.java
@@ -0,0 +1,50 @@
+/*
+ * Java Scan, a library for scanning and configuring HBM devices.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (C) Stephan Gatzka
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import com.hbm.devices.scan.announce.AnnounceReceiver;
+
+public class AnnounceReceiverTest {
+
+    @Test
+    public void intantiation() {
+        try {
+            AnnounceReceiver ar = new AnnounceReceiver();
+            assertNotNull("Could not instantiate AnnounceReceiver", ar);
+        } catch (IOException e) {
+            fail("Got IOException while instantiating AnnounceReceiver");
+        }
+    }
+}
