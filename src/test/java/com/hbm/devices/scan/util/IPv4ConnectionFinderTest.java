7d4a6a3ab2efecc4bc13cd171df016d4e040245b
Stephan Gatzka <stephan.gatzka@gmail.com>
2015-03-01 15:27:58 +0100

    Add unit test for calculating a network prefix from netmask address.

--- src/test/java/com/hbm/devices/scan/util/IPv4ConnectionFinderTest.java
+++ src/test/java/com/hbm/devices/scan/util/IPv4ConnectionFinderTest.java
@@ -1,5 +1,6 @@
 package com.hbm.devices.scan.util;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -14,12 +15,12 @@ public class IPv4ConnectionFinderTest {
     @Test
     public void sameNetTest() {
         try {
-        InetAddress announceAddress = InetAddress.getByName("172.19.1.2");
-        int announcePrefix = 16;
-        InetAddress interfaceAddress = InetAddress.getByName("172.19.1.2");
-        int interfacePrefix = 16;
+            InetAddress announceAddress = InetAddress.getByName("172.19.1.2");
+            int announcePrefix = 16;
+            InetAddress interfaceAddress = InetAddress.getByName("172.19.1.2");
+            int interfacePrefix = 16;
 
-        assertTrue("Addresses should be in the same net", IPv4ConnectionFinder.sameNet(announceAddress, announcePrefix, interfaceAddress, interfacePrefix));
+            assertTrue("Addresses should be in the same net", IPv4ConnectionFinder.sameNet(announceAddress, announcePrefix, interfaceAddress, interfacePrefix));
         } catch (UnknownHostException e) {
             fail("name resolution failed");
         }
@@ -28,12 +29,24 @@ public class IPv4ConnectionFinderTest {
     @Test
     public void notSameNetTest() {
         try {
-        InetAddress announceAddress = InetAddress.getByName("172.19.1.2");
-        int announcePrefix = 16;
-        InetAddress interfaceAddress = InetAddress.getByName("172.19.1.2");
-        int interfacePrefix = 15;
+            InetAddress announceAddress = InetAddress.getByName("172.19.1.2");
+            int announcePrefix = 16;
+            InetAddress interfaceAddress = InetAddress.getByName("172.19.1.2");
+            int interfacePrefix = 15;
 
-        assertFalse("Addresses should not be in the same net", IPv4ConnectionFinder.sameNet(announceAddress, announcePrefix, interfaceAddress, interfacePrefix));
+            assertFalse("Addresses should not be in the same net", IPv4ConnectionFinder.sameNet(announceAddress, announcePrefix, interfaceAddress, interfacePrefix));
+        } catch (UnknownHostException e) {
+            fail("name resolution failed");
+        }
+    }
+    
+    @Test
+    public void calculatePrefixTest() {
+        try {
+            assertEquals("prefix conversion failed", IPv4ConnectionFinder.calculatePrefix(InetAddress.getByName("0.0.0.0")), 0);
+            assertEquals("prefix conversion failed", IPv4ConnectionFinder.calculatePrefix(InetAddress.getByName("255.255.255.255")), 32);
+            assertEquals("prefix conversion failed", IPv4ConnectionFinder.calculatePrefix(InetAddress.getByName("255.63.0.0")), 14);
+            assertEquals("prefix conversion failed", IPv4ConnectionFinder.calculatePrefix(InetAddress.getByName("127.0.0.0")), 7);
         } catch (UnknownHostException e) {
             fail("name resolution failed");
         }
