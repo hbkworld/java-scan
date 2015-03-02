1a9b9b8885c8db275e046e57d53fdb4b3774bb2a
Stephan Gatzka <stephan.gatzka@gmail.com>
2015-03-02 20:41:04 +0100

    Add test to find a connectable IP address from an announce.

--- src/test/java/com/hbm/devices/scan/util/IPv4ConnectionFinderTest.java
+++ src/test/java/com/hbm/devices/scan/util/IPv4ConnectionFinderTest.java
@@ -2,6 +2,7 @@ package com.hbm.devices.scan.util;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -9,8 +10,19 @@ import org.junit.Test;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.LinkedList;
+import java.util.Observer;
+import java.util.Observable;
+
+import com.hbm.devices.scan.FakeMessageReceiver;
+import com.hbm.devices.scan.messages.Announce;
+import com.hbm.devices.scan.messages.AnnounceDeserializer;
+import com.hbm.devices.scan.messages.MissingDataException;
+
 
 public class IPv4ConnectionFinderTest {
+
+    private Announce announce;
 
     @Test
     public void sameNetTest() {
@@ -47,6 +59,40 @@ public class IPv4ConnectionFinderTest {
             assertEquals("prefix conversion failed", IPv4ConnectionFinder.calculatePrefix(InetAddress.getByName("255.255.255.255")), 32);
             assertEquals("prefix conversion failed", IPv4ConnectionFinder.calculatePrefix(InetAddress.getByName("255.63.0.0")), 14);
             assertEquals("prefix conversion failed", IPv4ConnectionFinder.calculatePrefix(InetAddress.getByName("127.0.0.0")), 7);
+        } catch (UnknownHostException e) {
+            fail("name resolution failed");
+        }
+    }
+
+    @Test
+    public void findIPAddressInList() {
+        LinkedList<NetworkInterfaceAddress> list = new LinkedList<NetworkInterfaceAddress>();
+        try {
+            list.push(new NetworkInterfaceAddress(InetAddress.getByName("10.1.2.3"), 8));
+            list.push(new NetworkInterfaceAddress(InetAddress.getByName("172.19.1.2"), 16));
+            list.push(new NetworkInterfaceAddress(InetAddress.getByName("192.168.4.5"), 24));
+            IPv4ConnectionFinder finder = new IPv4ConnectionFinder(list);
+
+            announce = null;
+            FakeMessageReceiver fsmmr = new FakeMessageReceiver();
+            AnnounceDeserializer parser = new AnnounceDeserializer();
+            fsmmr.addObserver(parser);
+            parser.addObserver(new Observer() {
+                public void update(Observable o, Object arg) {
+                    if (arg instanceof Announce) {
+                        announce = (Announce) arg;
+                    } 
+                }
+            });
+
+            fsmmr.emitSingleCorrectMessage();
+            assertNotNull("No Announce object after correct message", announce);
+            try {
+                InetAddress addr = finder.getConnectableAddress(announce);
+                assertNotNull("Device not connectable", addr);
+            } catch (MissingDataException e) {
+                fail("some information in anounce missing");
+            }
         } catch (UnknownHostException e) {
             fail("name resolution failed");
         }
